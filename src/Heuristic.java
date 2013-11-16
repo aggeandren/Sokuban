89af427e502ad342328c880ee30120b1c384a916
roghult <alexander.roghult@gmail.com>
2013-11-16 16:23:33 +0100

    Added Heuristic.java

---
+++ src/Heuristic.java
@@ -0,0 +1,24 @@
+
+public class Heuristic {
+	
+	public static int getHeuristicValue(GameState gs) {
+		int value = 0;
+		for(Cell box : gs.boxState) {
+			int smallestDistance = Integer.MAX_VALUE;
+			int distance = 0; 
+			for(Cell goal : gs.goals) {
+				distance = manhattanDistance(box, goal);
+				if(distance < smallestDistance) {
+					smallestDistance = distance;
+				}
+			}
+			value += smallestDistance;
+		}
+		return value;
+	}
+	
+	private static int manhattanDistance(Cell a, Cell b) {
+		return (b.getX() - a.getX()) + (b.getY() - a.getY());
+	}
+
+}
